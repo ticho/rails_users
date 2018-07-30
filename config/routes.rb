ce9427e26272bffb15540f9fd1bb3b1ae85d4868
ticho <tibofolly@gmail.com>
2018-07-30 19:04:09 +0200

    plural controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # get 'static_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'users' => 'user#all'
-  get 'users/new' => 'user#new'
-  post 'users/new' => 'user#create'
+  get 'users' => 'users#all'
+  get 'users/new' => 'users#new'
+  post 'users/new' => 'users#create'
 
-  get 'users/:id' => 'user#show'
+  get 'users/:id' => 'users#show'
 
   root 'static_pages#index'
 end
