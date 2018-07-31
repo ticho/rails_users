a3257408e53c64ed383bd0b25a58c879e1fe8a23
ticho <tibofolly@gmail.com>
2018-07-31 23:08:30 +0200

    bootstrap + username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: {in: 3..10}
+  validates :username, presence: true, uniqueness: true, length: {in: 3..10}, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates :bio, presence: true, length: {maximum: 500}
 end
